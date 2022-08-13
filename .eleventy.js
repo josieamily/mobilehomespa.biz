5f40cd76c6cfbd95ebc4410dffcf6d560d5159aa
Alejandro Sedano <alejandro@sedano.dev>
2022-08-13 19:31:28 +0200

    Use eleventy site generator

---
+++ .eleventy.js
@@ -0,0 +1,15 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addWatchTarget("./src/sass/");
+    eleventyConfig.addPassthroughCopy({ "./src/css": "assets/css" });
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addPassthroughCopy("./src/manifest.json");
+    return {
+      dir: {
+        input: "src",
+        output: "public"
+      },
+      htmlTemplateEngine: false
+    }
+  };
+  
